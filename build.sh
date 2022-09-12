dccb8f60a9a17c8eddfb524250ba06ad0ab40309
Sai Cherukumilli <cherukumilli.sai@gmail.com>
2022-09-12 10:01:35 -0400

    try build pass

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
-cd ..
-git clone https://github.com/matrix-org/matrix-js-sdk.git
-pushd matrix-js-sdk
-yarn link
-yarn install
-popd
+# cd ..
+# git clone https://github.com/matrix-org/matrix-js-sdk.git
+# pushd matrix-js-sdk
+# yarn link
+# yarn install
+# popd
 
-cd ..
-git clone https://github.com/matrix-org/matrix-react-sdk.git
-pushd matrix-react-sdk
-yarn link
-yarn link matrix-js-sdk
-yarn install
-popd
+# cd ..
+# git clone https://github.com/matrix-org/matrix-react-sdk.git
+# pushd matrix-react-sdk
+# yarn link
+# yarn link matrix-js-sdk
+# yarn install
+# popd
 
 yarn clean && yarn build:genfiles && yarn build:bundle
